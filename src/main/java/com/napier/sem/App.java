f86eeb3867e0cb4a0ce1bb9277f3732ff12f87fb
Goddey <40655471@1234567>
2024-10-04 12:44:04 +0100

    corrected code

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class App
 {
-    public void main(String[] args)
+    public static void main(String[] args)
     {
         // Create new Application
         App a = new App();
